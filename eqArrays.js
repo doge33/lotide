d2f4e662c09faab0158d6168a3dc596a71dc447f
doge33 <sandylx33@gmail.com>
2020-08-19 00:01:37 +0000

    compare arrays to exact match

---
+++ eqArrays.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🧐Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`💯💯💯Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2 [i]) {
+      return false;
+    }
+  } return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+
+
+/*
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+
+    }
+  }
+}
+*/
