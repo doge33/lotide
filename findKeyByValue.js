09273adcd0841455c31a5b86d6873d738d50df22
doge33 <sandylx33@gmail.com>
2020-08-19 23:37:43 +0000

    find key in object by matching values

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🧐Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`💯💯💯Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function (object, value) {
+  //iterate through the keys of object
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+/* test cases
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  documentary: "Filthy Rich",
+  crime: "CSI"
+};
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "CSI"), "crime");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "House"), undefined);
+*/
