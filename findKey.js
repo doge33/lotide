97897d8415c9404a2e7208c42443f21b9f9c8f62
doge33 <sandylx33@gmail.com>
2020-08-21 00:53:51 +0000

    perform callback on keys of object & return key

---
+++ findKey.js
@@ -0,0 +1,30 @@
+/*
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🧐Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`💯💯💯Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+*/
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key]) === true) {
+      return key;
+    }
+  }
+}
+
+/*
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log(result);
+*/
