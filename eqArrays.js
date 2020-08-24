cdfa48776f97bb538710a94452174e7c0398bff4
doge33 <sandylx33@gmail.com>
2020-08-24 17:38:53 +0000

    separated eqArrays and its tests(still utilizing assertEqual)

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🧐Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`💯💯💯Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+
 
 const eqArrays = function(arr1, arr2) {
 
@@ -18,13 +12,6 @@ const eqArrays = function(arr1, arr2) {
   } return true;
 };
 
-/*
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-*/
 
